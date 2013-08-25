ac48947588efd58beb8ad9b57501e7855c98082c
sthor <sthors10@student.aau.dk>
2013-08-25 18:50:00 +0200

    added a plot choise. --plot_enable=1 or 0

--- riddler_plot.py
+++ riddler_plot.py
@@ -19,7 +19,25 @@ class plot:
                 for node,d in df.groupby('node'):
                     ranks = d['ranks'].apply(lambda x: pd.Series(eval(x)))
                     #print "ranks:", ranks
-                    ranks.mean().plot(label=node)
+                    ranks.mean().plot(kind="bar",label=node)
+                    #break
+                #plt.legend("coding","symbols","field")
+                #break
+            plt.show()
+            
+    def plot_last_received_seq_num(self): #TASK! not finished
+            if not self.df:
+                return
+            #print self.df
+            
+            groups = self.df.groupby(['received_packets', 'field'])
+            for (symbols,field),df in groups:
+                plt.figure()
+                plt.title(str((symbols,field)))
+                for node,d in df.groupby('node'):
+                    ranks = d['ranks'].apply(lambda x: pd.Series(eval(x)))
+                    #print "ranks:", ranks
+                    ranks.mean().plot(kind="bar",label=node)
                     #break
                 #plt.legend("coding","symbols","field")
                 #break
